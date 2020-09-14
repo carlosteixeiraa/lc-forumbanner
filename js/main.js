3a0bd2b698f315bf5d287c8da58ab8c20b4668d7
Carlos Teixeira <carlosteixeira.email@gmail.com>
2020-09-14 13:19:49 +0100

    jpeg support added, changed banner size, changed tag font weight

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ var node = document.getElementById('signDiv');
 var btn = document.getElementById('download');
   
   btn.onclick = function() {
-    domtoimage.toBlob(document.getElementById('signDiv'))
-      .then(function(blob) {
+    domtoimage.toJpeg(document.getElementById('signDiv'), {quality: 1})
+      .then(function(dataURL) {
       
         if(user) {
           var imageName = 'signature-' + user.toLowerCase(); + '.png';
-          window.saveAs(blob, imageName);
+          window.saveAs(dataURL, imageName);
         } else {
           // #56A1DA
           swal("You forgot something!", "Please insert a username before clicking submit.");
@@ -27,8 +27,6 @@ $("#username").on("change keyup paste", function(){
     $("#signUsername h4").text(toChange);
   } else {
     swal("Maximum characters!", "You can only use up to 15 characters.");
-    var newVal = toChange.slice(0, -1);
-    $("#signUsername h4").text(toChange);
   }
   
 
