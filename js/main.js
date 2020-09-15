ccb1921b10c8ee64ee929195bf23ce174bd02f89
Carlos Teixeira <carlosteixeira.email@gmail.com>
2020-09-15 22:10:08 +0100

    auto black or white for tag color

--- js/main.js
+++ js/main.js
@@ -34,8 +34,8 @@ $("#username").on("change keyup paste", function(){
 
 
 function checkHex (hex) {
+  
   var reg = /^#[0-9A-F]{6}$/i; 
-
   return reg.test(hex);
 
 }
@@ -65,6 +65,14 @@ $("#tagcolor").focus(function() {
 
 });
 
+function darkOrLight(bgColor, lightColor, darkColor) {
+  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16); 
+  var g = parseInt(color.substring(2, 4), 16); 
+  var b = parseInt(color.substring(4, 6), 16); 
+  return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+    darkColor : lightColor;
+}
 
 $("#tagcolor").on("change keyup paste", function(){
 
@@ -73,10 +81,13 @@ $("#tagcolor").on("change keyup paste", function(){
     if(checkHex(toChange) && toChange.length == 7) {
       $(".colorprev").css('color', toChange);
       $("#signTag h5").css('background', toChange);
+      var result = darkOrLight(toChange, '#FFFFFF', '#000000');
+      $("#signTag h5").css('color', result);
     } else if(toChange.length == 7) {
       swal("Wrong colorcode!", "Please use a valid hex colorcode.");
       $('#tagcolor').val('#');
     }
 
-
 });
+
+
